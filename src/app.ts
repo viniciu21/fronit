c8c9ccb5d8ba9436a74d0ef3a729cce7d3515409
ubuntovini <viniciusoliveiras12@hotmail.com.br>
2021-03-15 19:05:58 -0300

    feat: adding setup

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import express from 'express'
+
+const app = express();
+
+app.listen(8080, () => {
+    console.log('The application is listening on port 3000!');
+});
---
+++ test/functional/forecast.test.ts
@@ -0,0 +1,5 @@
+describe('Beach forecast functional tests', () => {
+  it('should return a forecast with just a few times', () => {
+    expect(1).toBe(1);
+  });
+});
